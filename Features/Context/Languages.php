6ee110628f5d8c3637b24a05ba0b6ebaac6bdc3b
Ricardo Sousa <ricardo.sousa@cleverti.com>
2015-12-23 12:00:09 +0000

    add some scenarios

--- Features/Context/Languages.php
+++ Features/Context/Languages.php
@@ -3,26 +3,21 @@
 namespace BDDTestBundle\Features\Context;
 
 use EzSystems\PlatformUIBundle\Features\Context\PlatformUI;
+use EzSystems\BehatBundle\Context\Browser\SubContext;
 use Behat\Gherkin\Node\TableNode;
 
 class Languages extends PlatformUI {
 
     /**
-     * @Given I click on the button :button
+     * @Given I see the Languages page
      */
-    public function iClickButton($button) {
-        $this->clickElementByText($button, 'button');
+    public function iSeeTheLanguagesPage() {
+        $this->sleep();
+        $this->iSeeTitle('Languages');
     }
 
     /**
-     * @Given I click on the link :link
-     */
-    public function iClickLink($link) {
-        $this->clickElementByText($link, 'a');
-    }
-
-    /**
-     * @Then I should see the newly created language with:
+     * @Then I should see the newly created/edited language with:
      */
     public function iSeeTheLanguage(TableNode $fields) {
         foreach ($fields as $field) {
@@ -37,9 +32,68 @@ class Languages extends PlatformUI {
 
         if($languageName != $tableCols[0]->getText() ||
            $languageCode != $tableCols[1]->getText() ||
-           //$isEnabled != $tableCols[3]->isChecked()) { // WTF???? _> returns false when the checkbox is checked...
            $isEnabled != ($tableCols[3]->getAttribute('checked') == null)) {
             throw new \Exception("Language '$languageName' couldn't be created!");
+        }
+    }
+
+    /**
+     * @Given I edit the language with name :languageName
+     */
+    public function iEditTheLanguage($languageName) {
+        $tableRow = $this->getElementContainsText($languageName, '.ez-selection-table-row');
+        if(!$tableRow) {
+            throw new \Exception("Couldn't find '$languageName'!");
+        }
+        $editButton = $tableRow->find('css', '.ez-button');
+        $editButton->click();
+    }
+
+    /**
+     * @Given I delete the language with name :languageName
+     */
+    public function iDeleteTheLanguage($languageName) {
+        $tableRow = $this->getElementContainsText($languageName, '.ez-selection-table-row');
+        if(!$tableRow) {
+            throw new \Exception("Couldn't find '$languageName'!");
+        }
+        $deleteButton = $tableRow->find('css', '#ezrepoforms_language_delete_delete');
+        $deleteButton->click();
+    }
+
+    /**
+     * @Then I shouldn't see the previously created language with:
+     */
+    public function iDontSeeTheLanguage(TableNode $fields) {
+        foreach ($fields as $field) {
+            $languageName = $field['Language name'];
+            $languageCode = $field['Language code'];
+            $isEnabled = $field['Enabled'];
+        }
+        $isEnabled = filter_var($isEnabled, FILTER_VALIDATE_BOOLEAN);
+
+        $tableRow = $this->getElementContainsText($languageName, '.ez-selection-table-row');
+        if(!$tableRow) {
+            // language was successfully deleted
+            return;
+        }
+        $tableCols = $tableRow->findAll('css', 'td');
+
+        if($languageName == $tableCols[0]->getText() &&
+           $languageCode == $tableCols[1]->getText() &&
+           $isEnabled == ($tableCols[3]->getAttribute('checked') == null)) {
+           throw new \Exception("Language '$languageName' couldn't be deleted!");
+        }
+    }
+
+    /**
+     * @Then I verify that the :textfield text field is disabled
+     */
+    public function iVerifyTextFieldDisabled($textField) {
+        $container = $this->getElementContainsText($textField, '.pure-control-group');
+        $textField = $container->find('css', 'input');
+        if(!$textField->hasAttribute('disabled')) {
+            throw \Exception("Text field '$textField' is not disabled!");
         }
     }
 }
--- Features/languages.feature
+++ Features/languages.feature
@@ -1,16 +1,44 @@
-Feature: Create a new language
+Feature: Perform languages admin related operations
 
-    @javascript @run
-    Scenario: I want to create a new language in Administration dashboard
-        Given I go to PlatformUI app with username "admin" and password "publish"
+    Background:
+        Given I am logged in as an 'Administrator' in PlatformUI
         And I click on the navigation zone "Admin Panel"
         And I click on the navigation item "Languages"
-        And I click on the link "Create a new language"
+
+    @javascript
+    Scenario: Verify the existence of Languages page and it's content
+        Given I see the Languages page
+        Then I see the following limitations fields:
+            | Language name   | Language code | Language ID | Enabled |
+        Then I can see "Create a new language" link
+
+    @javascript
+    Scenario: Create a new language
+        Given I click on the "Create a new language" link
         When I fill in "Name" with "NewLanguageTest"
         And I fill in "Language code" with "NLT"
         And I check "Enabled" checkbox
-        And I click on the button "Save"
+        And I click on the "Save" button
         And I click on the navigation item "Languages"
         Then I should see the newly created language with:
             | Language name   | Language code | Enabled |
             | NewLanguageTest | NLT           | true    |
+
+    @javascript
+    Scenario: Edit an existing language
+        Given I edit the language with name "NewLanguageTest"
+        When I fill in "Name" with "LanguageEditTest"
+        Then I verify that the "Language code" text field is disabled
+        And I check "Enabled" checkbox
+        And I click on the "Save" button
+        And I click on the navigation item "Languages"
+        Then I should see the newly edited language with:
+            | Language name    | Language code | Enabled |
+            | LanguageEditTest | NLT           | true    |
+
+    @javascript
+    Scenario: Delete an existing language
+        Given I delete the language with name "LanguageEditTest"
+        Then I shouldn't see the previously created language with:
+            | Language name    | Language code | Enabled |
+            | LanguageEditTest | NLT           | true    |
