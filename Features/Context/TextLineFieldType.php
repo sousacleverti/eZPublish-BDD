1b4c3505c2222195a798a9fd0f7533e04ba37ae1
Ricardo Sousa <ricardo.sousa@cleverti.com>
2015-12-23 16:44:48 +0000

    Add context file

---
+++ Features/Context/TextLineFieldType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace BDDTestBundle\Features\Context;
+
+use EzSystems\PlatformUIBundle\Features\Context\PlatformUI;
+use Behat\Gherkin\Node\TableNode;
+
+class TextLineFieldType extends PlatformUI {
+
+}
